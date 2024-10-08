9486572ec7e212ceeb92769c3f40b6e0754cf9ee
SalaiChangeWook <uchiasadara6@gmail.com>
2024-10-08 23:28:02 +0630

    Pre-release API code

---
+++ API/Demo.WebApi/IoC/ServiceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Demo.WebApi.Models;
+
+namespace Demo.WebApi.IoC
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddDemoApiClientService(this IServiceCollection services,
+           Action<ApiClientOptions> options)
+        {
+            services.Configure(options);
+            services.AddSingleton(provider =>
+            {
+                var options = provider.GetRequiredService<IOptions<ApiClientOptions>>().Value;
+                return new DemoApiClientService(options);
+            });
+
+        }
+    }
+}
